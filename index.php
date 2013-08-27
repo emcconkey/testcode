58900045f1b02b23ecb9a1970d31131bed3a79a9
emcconkey <eric@ericmcconkey.com>
2013-08-26 23:14:25 -0600

    Testdev changes

--- index.php
+++ index.php
@@ -9,9 +9,15 @@ function test2() {
 	echo "Testing function 2";
 }
 
+function testdev_function1() {
+	echo "Testing testdev";
+}
 
 test1();
 echo "<br />";
 test2();
+
+echo "<br />";
+testdev_function1();
 
 ?>
