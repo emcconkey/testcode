1f238c00c17e05e185f2b4964da18caa77cdc9e1
emcconkey <eric@ericmcconkey.com>
2013-08-26 23:12:33 -0600

    Added new function

--- index.php
+++ index.php
@@ -9,9 +9,18 @@ function test2() {
 	echo "Testing function 2";
 }
 
+function ericdev_function1() {
+	echo "ericdev1";
+}
 
 test1();
+
 echo "<br />";
+
 test2();
+
+echo "<br />";
+
+ericdev_function1();
 
 ?>
